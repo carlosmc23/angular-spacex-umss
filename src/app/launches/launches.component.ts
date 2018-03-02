25b3e3f6ac655c29bcb0e5852cf987745b8cd35a
luixaviles <luis.avilesz@gmail.com>
2018-03-01 21:56:17 -0400

    Usango ngFor

--- src/app/launches/launches.component.ts
+++ src/app/launches/launches.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Launch } from '../model/launch';
 
 @Component({
   selector: 'asu-launches',
@@ -7,9 +8,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LaunchesComponent implements OnInit {
 
+  launches: Launch[];
+
   constructor() { }
 
+  // Inicializacion del Componente
   ngOnInit() {
+    this.launches = [
+      {
+        launch_date_utc: '2018-02-22T14:17:00Z',
+        rocket: {
+          rocket_id: 'Falcon 9',
+          rocket_name: 'Falcon 9'
+        }
+      },
+      {
+        launch_date_utc: '2018-01-22T14:17:00Z',
+        rocket: {
+          rocket_id: 'Falcon 90',
+          rocket_name: 'Falcon 90'
+        }
+      },
+
+      {
+        launch_date_utc: '2018-01-22T14:17:00Z',
+        rocket: {
+          rocket_id: 'Falcon 90',
+          rocket_name: 'Falcon 90'
+        }
+      },
+      {
+        launch_date_utc: '2018-01-22T14:17:00Z',
+        rocket: {
+          rocket_id: 'Falcon 90',
+          rocket_name: 'Falcon 90'
+        }
+      },
+      {
+        launch_date_utc: '2018-01-22T14:17:00Z',
+        rocket: {
+          rocket_id: 'Falcon 90',
+          rocket_name: 'Falcon 90'
+        }
+      },
+      {
+        launch_date_utc: '2018-01-22T14:17:00Z',
+        rocket: {
+          rocket_id: 'Falcon 90',
+          rocket_name: 'Falcon 90'
+        }
+      }
+
+    ];
   }
 
 }
